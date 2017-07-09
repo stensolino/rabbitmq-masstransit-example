8a3afd63f765cde725643745b234b9c60d884975
Adnan Mulalic <stensolino@gmail.com>
2017-07-09 13:27:01 +0200

    Add consumer

---
+++ rabbitmq-masstransit-example.Consumer/PollResultConsumer.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using rabbitmq_masstransit_example.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabbitmq_masstransit_example.Consumer
+{
+    class PollResultConsumer : IConsumer<PollResult>
+    {
+        public async Task Consume(ConsumeContext<PollResult> context)
+        {
+            await Console.Out.WriteLineAsync($"Updating poll result: {context.Message.Name}");
+
+            // update poll result logic
+        }
+    }
+}
---
+++ rabbitmq-masstransit-example.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using GreenPipes;
+using MassTransit;
+using rabbitmq_masstransit_example.Consumer.Properties;
+using rabbitmq_masstransit_example.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabbitmq_masstransit_example.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var busControl = ConfigureBus();
+            busControl.Start();
+        }
+
+        static IBusControl ConfigureBus()
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg => {
+                var host = cfg.Host(new Uri($"rabbitmq://{Settings.Default.RabbitMQUri}"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                cfg.ReceiveEndpoint(host, typeof(PollResult).Name, e => e.Consumer<PollResultConsumer>());
+            });
+        }
+    }
+}
